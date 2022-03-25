1581addbe3f00115722d98a0925a32eea5997d58
Mati1432 <mateuszsliwinski9@gmail.com>
2022-03-25 21:53:48 +0100

    change url and add migration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 """Urls files."""
 # Django
-from django.urls import include
+from django.urls import include, re_path
 from django.urls import path
 
 # Project
@@ -8,6 +8,6 @@ from accounts import views
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
-    path(r'^patent-sign-up/', views.PatientSignUp.as_view(), name='patient-sign-up'),
-    path(r'^doctor-sign-up/', views.DoctorSignUp.as_view(), name='doctor-sign-up'),
+    path(r'patent-sign-up/', views.PatientSignUp.as_view(), name='patient-sign-up'),
+    path(r'doctor-sign-up/', views.DoctorSignUp.as_view(), name='doctor-sign-up'),
 ]
