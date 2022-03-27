5818f01b17ddeec65a3dd578df02f5f451db9bfa
Mati1432 <mateuszsliwinski9@gmail.com>
2022-03-27 14:55:27 +0200

    Add VisitForm

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,14 @@
 """Forms files."""
+from core.models import Visit
+
+
+class VisitForm(forms.ModelForm):  # noqa D101
+
+    class Meta:  # noqa D106
+        model = Visit
+        fields = [
+            'doctor',
+            'specialization',
+            'date_visit',
+            'visit',
+        ]
